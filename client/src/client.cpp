8b3645a31e8a15a9ef7a2e5c154b87b973571b1b
orbhandari <orbhandari@connect.ust.hk>
2026-01-08 20:30:01 +0800

    others: add clang tidy and clean up repo structure
    - clang tidy is a linter to help us write modern cpp
    - clean up some weird subdirectory structure like
    client/include/client/utils.h to client/include/utils.h

--- client/src/client.cpp
+++ client/src/client.cpp
@@ -2,7 +2,7 @@
 // Created by josh on 9/26/25.
 //
 
-#include "client/utils.h"
+#include "utils.h"
 
 int main() {
     hello_world();
--- client/src/utils.cpp
+++ client/src/utils.cpp
@@ -2,9 +2,9 @@
 // Created by josh on 9/26/25.
 //
 
-#include "client/utils.h"
+#include "utils.h"
 #include <iostream>
 
 void hello_world() {
-    std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World!" << '\n';
 }
