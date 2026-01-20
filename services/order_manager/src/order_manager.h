53142c91221857ab7e8d28a66e3d60900bc7f6f6
GC <phchoy@connect.ust.hk>
2026-01-21 05:09:30 +0800

    Updated Cmake list for OM

--- services/order_manager/src/order_manager.h
+++ services/order_manager/src/order_manager.h
@@ -2,9 +2,13 @@
 #define ELSA_FYP_ORDER_MANAGER_H
 
 #include "balance_checker.h"
+#include "transport/websocket_server.h"
+
+using WebsocketManagerServer = transport::WebsocketManagerServer;
 
 class OrderManager {
   private:
+    WebsocketManagerServer ws_server = WebsocketManagerServer(67);
     BalanceChecker balance_checker = BalanceChecker();
 };
 
