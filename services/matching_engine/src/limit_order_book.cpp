cb78cfa05fe2f503d08b61d317f969d3004c5beb
GC <phchoy@connect.ust.hk>
2026-01-21 05:53:19 +0800

    Renamed variables in match_order function

--- services/matching_engine/src/limit_order_book.cpp
+++ services/matching_engine/src/limit_order_book.cpp
@@ -31,37 +31,36 @@ void LimitOrderBook::match_order(std::map<int, std::list<Order>>& near_side,
                                  std::map<int, std::list<Order>>& far_side, int price, int quantity,
                                  int order_id, Side side) {
     while (!far_side.empty() && quantity > 0) {
-        const auto best_entry = (side == Side::Bid) ? std::prev(far_side.end()) : far_side.begin();
+        const auto best_level = (side == Side::Bid) ? std::prev(far_side.end()) : far_side.begin();
 
-        const auto& best_entry_price = best_entry->first;
-        if ((side == Side::Bid && price < best_entry_price) ||
-            (side == Side::Ask && price > best_entry_price)) {
+        const auto& best_level_price = best_level->first;
+        if ((side == Side::Bid && price < best_level_price) ||
+            (side == Side::Ask && price > best_level_price)) {
             break;
         }
 
-        auto& best_entry_orders = best_entry->second;
-        while (quantity > 0 && !best_entry_orders.empty()) {
-            auto& front_order = best_entry_orders.front();
+        auto& best_level_orders = best_level->second;
+        while (quantity > 0 && !best_level_orders.empty()) {
+            auto& front_order = best_level_orders.front();
             if (const auto order_quantity = front_order.get_quantity();
                 quantity >= order_quantity) {
                 quantity -= order_quantity;
-                order_id_map.erase(best_entry_orders.front().get_order_id());
-                best_entry_orders.pop_front();
+                order_id_map.erase(best_level_orders.front().get_order_id());
+                best_level_orders.pop_front();
             } else {
                 front_order.fill(quantity);
                 quantity = 0;
             }
         }
 
-        if (best_entry_orders.empty()) {
-            far_side.erase(best_entry_price);
+        if (best_level_orders.empty()) {
+            far_side.erase(best_level_price);
         }
     }
 
     if (quantity > 0) {
         order_id_map[order_id] =
             near_side[price].emplace(near_side[price].end(), order_id, price, quantity, side);
-        ;
     }
 }
 
