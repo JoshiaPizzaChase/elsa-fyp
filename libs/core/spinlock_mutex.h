15a2ff5d0c48df159fa0b4c4793f049d643796ee
orbhandari <orbhandari@connect.ust.hk>
2026-02-01 22:04:40 +0800

    added spinlock mutex

---
+++ libs/core/spinlock_mutex.h
@@ -0,0 +1,25 @@
+#ifndef CORE_SPINLOCK_MUTEX_H
+#define CORE_SPINLOCK_MUTEX_H
+
+#include <atomic>
+
+/*
+ * C++ does not provide a native implementation for a spinlock.
+ */
+class SpinlockMutex {
+  private:
+    std::atomic_flag flag{ATOMIC_FLAG_INIT};
+
+  public:
+    void get_lock() {
+        while (!flag.test_and_set(std::memory_order_acquire)) {
+            // spin until we successfully set the value atomically
+        }
+    }
+
+    void unlock() {
+        flag.clear(std::memory_order_relaxed);
+    }
+};
+
+#endif // CORE_SPINLOCK_MUTEX_H
