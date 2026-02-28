b984d0b13fa223ffb9d75ae17c64408247416339
orbhandari <orbhandari@connect.ust.hk>
2026-02-28 23:57:00 +0800

    added default timestamp to schema

--- scripts/db_scripts/init_postgresql.sql
+++ scripts/db_scripts/init_postgresql.sql
@@ -1,3 +1,5 @@
+-- TODO: Function to update timestamp on modification
+
 -- Set up the tables
 -- 1. users
 CREATE TABLE IF NOT EXISTS users 
@@ -5,8 +7,8 @@ CREATE TABLE IF NOT EXISTS users
     user_id INT PRIMARY KEY,
     username VARCHAR UNIQUE NOT NULL,
     password VARCHAR NOT NULL,
-    created_ts TIMESTAMPTZ NOT NULL,
-    last_modified_ts TIMESTAMPTZ NOT NULL
+    created_ts TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified_ts TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 -- 2. servers
 CREATE TABLE IF NOT EXISTS servers 
@@ -14,8 +16,8 @@ CREATE TABLE IF NOT EXISTS servers
     server_id INT PRIMARY KEY,
     server_name VARCHAR UNIQUE NOT NULL,
     admin_id INT REFERENCES users(user_id),
-    created_ts TIMESTAMPTZ,
-    last_modified_ts TIMESTAMPTZ,
+    created_ts TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_modified_ts TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     active_tickers VARCHAR(10)[]
 );
 -- 3. whitelist
@@ -23,7 +25,7 @@ CREATE TABLE IF NOT EXISTS allowlist
 (
     server_id INT REFERENCES servers(server_id),
     user_id INT REFERENCES users(user_id),
-    created_ts TIMESTAMPTZ NOT NULL,
+    created_ts TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(server_id, user_id)
 );
 -- 4. balances
@@ -32,8 +34,8 @@ CREATE TABLE IF NOT EXISTS balances
     user_id INT REFERENCES users(user_id),
     symbol VARCHAR(10) NOT NULL,
     balance INT NOT NULL,
-    created_ts TIMESTAMPTZ NOT NULL,
-    modified_ts TIMESTAMPTZ NOT NULL,
+    created_ts TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modified_ts TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY(user_id, symbol)
 );
 
