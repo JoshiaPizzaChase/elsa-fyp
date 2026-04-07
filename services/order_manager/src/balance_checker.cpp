6e48cbcfd9cd94854423d12aa9bf601065142d4b
GC <phchoy@connect.ust.hk>
2026-04-08 02:27:37 +0800

    Updated error handling in Balance Checker and added unit tests

--- services/order_manager/src/balance_checker.cpp
+++ services/order_manager/src/balance_checker.cpp
@@ -2,7 +2,6 @@
 
 #include <boost/contract.hpp>
 #include <cassert>
-#include <format>
 
 namespace om {
 bool BalanceChecker::broker_id_exists(const std::string& broker_id) const {
@@ -11,49 +10,58 @@ bool BalanceChecker::broker_id_exists(const std::string& broker_id) const {
 
 bool BalanceChecker::broker_owns_ticker(const std::string& broker_id,
                                         const std::string& ticker) const {
-    boost::contract::check c = boost::contract::public_function(this).precondition(
-        [&] { BOOST_CONTRACT_ASSERT(broker_id_exists(broker_id)); });
+    boost::contract::check c = boost::contract::public_function(this).precondition([&] {
+        BOOST_CONTRACT_ASSERT(broker_id_exists(broker_id));
+        BOOST_CONTRACT_ASSERT(!ticker.empty());
+    });
 
     return balance_map.at(broker_id).contains(ticker);
 }
 
 void BalanceChecker::update_balance(const std::string& broker_id, const std::string& ticker,
                                     int delta) {
-    auto broker_it = balance_map.find(broker_id);
+    boost::contract::check c = boost::contract::public_function(this).precondition([&] {
+        BOOST_CONTRACT_ASSERT(!ticker.empty());
+        BOOST_CONTRACT_ASSERT(delta != 0);
+    });
 
     // Check if broker has record
-    if (broker_it != balance_map.end()) {
+    if (const auto broker_it = balance_map.find(broker_id); broker_it != balance_map.end()) {
         // Check if broker has ticker's record
-        auto ticker_it = broker_it->second.find(ticker);
-
-        if (ticker_it != broker_it->second.end()) {
+        if (const auto ticker_it = broker_it->second.find(ticker);
+            ticker_it != broker_it->second.end()) {
             // Check if broker has sufficient balance
             assert(has_sufficient_balance(broker_id, ticker, delta) &&
                    "Cannot deduct more balance than available");
 
             ticker_it->second += delta;
         } else {
-            assert(delta >= 0 && "Cannot create new account balance with negative delta");
-
             broker_it->second.emplace(ticker, delta);
         }
     } else {
-        assert(delta >= 0 && "Cannot create new account balance with negative delta");
-
         balance_map.emplace(broker_id, std::unordered_map<std::string, int>{{ticker, delta}});
     }
 }
 
 int BalanceChecker::get_balance(const std::string& broker_id, const std::string& ticker) const {
-    assert(broker_id_exists(broker_id) && "Balance record of Broker ID does not exist");
-    assert(broker_owns_ticker(broker_id, ticker) && "Broker does not have record of ticker");
-    return balance_map.at(broker_id).at(ticker);
+    int rtn;
+    boost::contract::check c = boost::contract::public_function(this)
+                                   .precondition([&] {
+                                       BOOST_CONTRACT_ASSERT(broker_id_exists(broker_id));
+                                       BOOST_CONTRACT_ASSERT(broker_owns_ticker(broker_id, ticker));
+                                   })
+                                   .postcondition([&] { BOOST_CONTRACT_ASSERT(rtn >= 0); });
+
+    return rtn = balance_map.at(broker_id).at(ticker);
 }
 
 bool BalanceChecker::has_sufficient_balance(const std::string& broker_id, const std::string& ticker,
                                             int delta) const {
-    assert(broker_id_exists(broker_id) && "Balance record of Broker ID does not exist");
-    assert(broker_owns_ticker(broker_id, ticker) && "Broker does not have record of ticker");
+    boost::contract::check c = boost::contract::public_function(this).precondition([&] {
+        BOOST_CONTRACT_ASSERT(broker_id_exists(broker_id));
+        BOOST_CONTRACT_ASSERT(broker_owns_ticker(broker_id, ticker));
+    });
+
     return get_balance(broker_id, ticker) + delta >= 0;
 }
 } // namespace om
